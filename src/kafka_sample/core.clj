35210bdc40fd63f55cb761478fdfbb3a6a2c3296
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-09-12 20:47:19 +0100

    minor modifications

--- src/kafka_sample/core.clj
+++ src/kafka_sample/core.clj
@@ -28,7 +28,8 @@
    :onyx.messaging/bind-addr "localhost"})
 
 (def workflow
-  [[:in :out]])
+  [[:in :identity]
+   [:identity :out]])
 
 (def n-peers (count (set (mapcat identity workflow))))
 
@@ -41,18 +42,22 @@
     :onyx/max-peers 1
     :onyx/batch-size 100
     :kafka/topic "test-data"
-    :kafka/group-id "onyx-consumer"
+    :kafka/group-id "onyx-consumer3"
     :kafka/zookeeper "127.0.0.1:2181"
     :kafka/offset-reset :smallest
-    :kafka/force-reset? false
+    :kafka/force-reset? true
     :kafka/deserializer-fn :onyx.tasks.kafka/deserialize-message-edn
     :kafka/wrap-with-metadata? false
     :onyx/doc "Reads messages from a Kafka topic"}
+    {:onyx/name :identity
+     :onyx/fn :clojure.core/identity
+     :onyx/batch-size 100
+     :onyx/type :function}
    {:onyx/name :out
     :onyx/plugin :onyx.test-helper/dummy-output
     :onyx/type :output
     :onyx/medium :null
-    :onyx/max-peers 1
+    :onyx/min-peers 1
     :onyx/batch-size 100
     :onyx/doc "Writes segments to dev null"}])
 
@@ -64,19 +69,19 @@
   [& args]
   (let [env         (onyx.api/start-env env-config)
         peer-group  (onyx.api/start-peer-group peer-config)
-        v-peers     (onyx.api/start-peers n-peers peer-group)
+        v-peers     (onyx.api/start-peers (* 2 n-peers) peer-group)
         job-id      (:job-id
                      (onyx.api/submit-job
                       peer-config
                       {:catalog catalog
                        :workflow workflow
                        :lifecycles lifecycles
-                       :task-scheduler :onyx.task-scheduler/balanced}))
-        _           (onyx.api/await-job-completion peer-config job-id)]
+                       :task-scheduler :onyx.task-scheduler/balanced}))]
+    (println job-id)
     (println "Stopping")
+    (onyx.api/await-job-completion peer-config job-id)
     (doseq [v-peer v-peers]
       (onyx.api/shutdown-peer v-peer))
     (onyx.api/shutdown-peer-group peer-group)
-
     (onyx.api/shutdown-env env)))
 
